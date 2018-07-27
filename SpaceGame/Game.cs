ffc111cec0e38cd67f19be640795452d6c9ab7ad
albyn0s <dmitryneff@gmail.com>
2018-07-27 22:46:13 +0500

    Class.cs -> Program.cs Created class Game.cs

--- SpaceGame/Game.cs
+++ SpaceGame/Game.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SpaceGame
 {
-    class Class1
+    class Game
     {
     }
 }
---
+++ SpaceGame/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceGame
+{
+    class Vector
+    {
+        public double X, Y;
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override bool Equals(object obj)
+        {
+            Vector v = obj as Vector;
+            if (v == null) return false; // Или можно создать исключение throw new InvalidCastException()
+        return (obj as Vector).X == X && (obj as Vector).Y == Y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1, 1);
+            Vector v2 = new Vector(1, 1);
+            Console.WriteLine(v1.Equals(v2));
+        }
+    }
+}
